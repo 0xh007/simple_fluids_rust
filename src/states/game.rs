e1b1b1b1fdd040592d27543164108f539ebc3d58
Victor Rogers <victor.r.rogers@protonmail.com>
2020-06-19 08:52:59 +0000

    added the game state

---
+++ src/states/game.rs
@@ -0,0 +1,71 @@
+use amethyst::{
+    core::{
+        math::Vector3,
+        Named,
+        Transform,
+    },
+    input::{
+        is_close_requested,
+        is_key_down,
+    },
+    prelude::*,
+    renderer::camera::Camera,
+    window::ScreenDimensions,
+    winit,
+};
+
+use crate::{
+    components::player::Player,
+    utils::camera_util::*,
+    utils::debug_util::*,
+    utils::player_util::*,
+    utils::sprite_util::*,
+    utils::tile_map_util::*,
+};
+
+pub struct GameState;
+
+impl SimpleState for GameState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+        let world = _data.world;
+        world.register::<Named>();
+        world.register::<Player>();
+
+        let map_sprite_sheet_handle = load_sprite_sheet(world, "assets/textures/dirt.png", "assets/textures/dirt.ron");
+
+        let (width, height) = {
+            let dim = world.read_resource::<ScreenDimensions>();
+            (dim.width(), dim.height())
+        };
+
+        let player = init_player(world);
+
+        let _camera = init_camera(
+            world,
+            player,
+            Transform::from(Vector3::new(0.0, 0.0, 1.1)),
+            Camera::standard_2d(width, height),
+        );
+
+        // Creating a test debug lines entity
+        let _ = create_debug_lines(world);
+
+        let map = create_tile_map(map_sprite_sheet_handle);
+
+        let _map_entity = create_map_entity(world, map);
+        println!("Created map");
+    }
+
+    fn handle_event(&mut self, data: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans {
+        let StateData { .. } = data;
+        if let StateEvent::Window(event) = &event {
+            if is_close_requested(&event) || is_key_down(&event, winit::VirtualKeyCode::Escape) {
+                Trans::Quit
+            } else {
+                Trans::None
+            }
+        } else {
+            Trans::None
+        }
+    }
+}
