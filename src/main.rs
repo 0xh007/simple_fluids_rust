abbfd6bed983527ae627917396b4c1672a9712ce
Victor Rogers <victor.r.rogers@protonmail.com>
2020-06-20 08:52:00 +0000

    game now launches

--- src/main.rs
+++ src/main.rs
@@ -23,32 +23,29 @@ mod utils;
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
-    let resources = application_root_dir()
-        .unwrap()
-        .into_os_string()
-        .into_string()
-        .unwrap()
-        + "/resources";
+    let app_root = application_root_dir()?;
+    let assets = app_root.join("assets");
+    let config = app_root.join("config");
 
-    let display_config_path = resources.clone() + "/config/display.ron";
-    let key_bindings_path = resources.clone() + "/config/input.ron";
+    let display_config = config.join("display.ron");
+    let key_bindings_config = config.join("input.ron");
 
     let game_data = GameDataBuilder::default()
         .with_bundle(TransformBundle::new())?
         .with_bundle(
             InputBundle::<StringBindings>::new()
-                .with_bindings_from_file(key_bindings_path)?
+                .with_bindings_from_file(key_bindings_config)?
         )?
         .with_bundle(RenderingBundle::<DefaultBackend>::new()
             .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)?
+                RenderToWindow::from_config_path(display_config)?
                     .with_clear([0.34, 0.36, 0.52, 1.0]),
             )
             .with_plugin(RenderDebugLines::default())
             .with_plugin(RenderFlat2D::default())
         )?;
 
-    let mut game = Application::build(resources, states::GameState)?.build(game_data)?;
+    let mut game = Application::build(assets, states::GameState)?.build(game_data)?;
     game.run();
 
     Ok(())
