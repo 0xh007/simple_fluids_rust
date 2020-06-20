5473612acc80c171ae3f589d3d7fa7bd01377146
Victor Rogers <victor.r.rogers@protonmail.com>
2020-06-20 09:53:53 +0000

    added more physics libraries

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,5 @@
 pub mod camera_util;
 pub mod debug_util;
 pub mod player_util;
-pub mod salva_util;
+pub mod rust_sim_util;
 pub mod sprite_util;
---
+++ src/utils/rust_sim_util.rs
@@ -0,0 +1,5 @@
+extern crate nalgebra as nalgebra;
+extern crate ncollide2d as ncollide2d;
+
+use nalgebra::{Isometry2, Point2, Point3, Vector2};
+use ncollide2d::shape::{Cuboid, ShapeHandle};
