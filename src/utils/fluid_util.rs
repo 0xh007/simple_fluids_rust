36d366abad85793b7378bb5503e3506cc3a7786d
Victor Rogers <victor.r.rogers@protonmail.com>
2020-06-22 09:18:38 +0000

    first water particle rendered

--- src/utils/fluid_util.rs
+++ src/utils/fluid_util.rs
@@ -1,17 +1,25 @@
 use amethyst::{
+    assets::Handle,
     core::Transform,
     ecs::Entity,
     prelude::*,
+    renderer::{SpriteRender, SpriteSheet},
 };
 
 use crate::components::fluid_world::FluidWorld;
 
-pub fn init_fluid_world(world: &mut World) -> Entity {
+pub fn init_fluid_world(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) -> Entity {
     let mut transform = Transform::default();
     transform.set_translation_xyz(0.0, 0.0, 0.1);
 
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle,
+        sprite_number: 0,
+    };
+
     world
         .create_entity()
+        .with(sprite_render)
         .with(FluidWorld::new(0.2, 0.0, 0.000001))
         .with(transform)
         .named("fluid world")
